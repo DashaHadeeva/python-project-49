8bac6f0acee8fbc9095bfb321abb4ba509aa0edf
DashaHadeeva <hadeeva.99@yandex.ru>
2023-02-08 16:09:29 +0500

    add

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-
-from brain_games.cli import welcome_user 
-import prompt 
+from brain_games.cli import welcome_user  
 
 def main():
-    welcome_user()
+    print('Welcome to the Brain Games!')
+    print(welcome_user())
 
 if __name__ == '__main__':
     main()
