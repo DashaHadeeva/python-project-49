7fda86bca9d02212614c24495bc8333fc8e6ff6b
DashaHadeeva <hadeeva.99@yandex.ru>
2023-02-13 19:28:33 +0500

    add

---
+++ brain_games/games/calc.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from random import randint
+from random import choice
+import operator
+
+
+def brain_calc():
+    number1 = randint(10, 100)
+    number2 = randint(1, 10)
+    operator_str = ['+', '-', '*']
+    operator_rand = choice(operator_str)
+    task = 'What is the result of the expression?'
+    question = f'Question: {number1} {operator_rand} {number2}'
+    if operator_rand == '+':
+        result = str(operator.add(number1, number2))
+    elif operator_rand == '-':
+        result = str(operator.sub(number1, number2))
+    else: 
+        result = str(operator.mul(number1, number2))
+    return result, task, question
+    
---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+from brain_games.games.calc import brain_calc
+from brain_games.brain_engine import brain_run
+
+
+def main():
+    brain_run(brain_calc)
+
+
+if __name__ == '__main__':
+    main()
