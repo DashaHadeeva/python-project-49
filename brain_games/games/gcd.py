cfa9e5cbd408868e66286f51c36aff9c88435627
DashaHadeeva <hadeeva.99@yandex.ru>
2023-02-20 20:24:21 +0500

    add

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from random import randint
+import math
+
+
+def brain_gcd():
+    number1 = randint(10, 99)
+    number2 = randint(10, 99)
+    task = 'Find the greatest common divisor of given numbers.'
+    question = (f'Question: {number1} {number2}')
+    result = str(math.gcd(number1, number2))
+    return result, task, question
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from brain_games.games.gcd import brain_gcd
+from brain_games.brain_engine import brain_run
+
+
+def main():
+    brain_run(brain_gcd)
+
+
+if __name__ == '__main__':
+    main()
