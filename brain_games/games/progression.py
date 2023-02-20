8ecc83499398e70cd1748e6055ea6224f4272354
DashaHadeeva <hadeeva.99@yandex.ru>
2023-02-20 20:47:17 +0500

    add

---
+++ brain_games/games/progression.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from random import randint
+
+
+def brain_progression():
+    start = randint(1, 20)
+    finish = randint(120, 150)
+    step = randint(1, 10)
+    position = randint(0, 9)
+    progression = range(start, finish, step)
+    list_progr = list(progression)
+    result = str(progression[position])
+    list_progr[position] = '..'
+    collect = ''
+    for i in list_progr[:10]:
+        collect = collect + ' ' + str(i)
+    question = f'Question: {collect}'
+    task = 'What number is missing in the progression?'
+    return result, task, question
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from brain_games.games.progression import brain_progression
+from brain_games.brain_engine import brain_run
+
+
+def main():
+    brain_run(brain_progression)
+
+
+if __name__ == "__main__":
+    main()
