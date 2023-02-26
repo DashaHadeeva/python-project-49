f15c3fb65a2c98953b9f4a244d57ce6e1156f6dc
DashaHadeeva <hadeeva.99@yandex.ru>
2023-02-27 00:30:29 +0500

    add

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -4,16 +4,13 @@ from random import randint
 
 def brain_progression():
     start = randint(1, 20)
-    finish = randint(120, 150)
     step = randint(1, 10)
-    position = randint(0, 9)
-    progression = range(start, finish, step)
-    list_progr = list(progression)
+    length = randint(5, 10)
+    progression = list(range(start, start + step * length, step))
+    position = randint(0, len(progression) - 1)
     result = str(progression[position])
-    list_progr[position] = '..'
-    collect = ''
-    for i in list_progr(randint(5,10)):
-        collect = collect + ' ' + str(i)
+    progression[position] = '..'
+    collect = ' '.join(map(str, progression))
     question = f'Question: {collect}'
     task = 'What number is missing in the progression?'
     return result, task, question
